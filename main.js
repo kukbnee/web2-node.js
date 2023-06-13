50e453102fc01c17799e9c2e75d0736ac792d9b1
kukbni <kukbnee@gmail.com>
2023-06-13 16:50:39 +0900

    template 모듈화

--- main.js
+++ main.js
@@ -2,40 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-const templeteHTML = (title, $fileList, $create, $update, $delete, body)=> {
-  return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${$fileList}
-      ${$create}
-      ${$update}
-      ${$delete}
-      ${body}
-      
-    </body>
-    </html>
-  `;
-};
-
-const templeteList = (files)=> {
-
-  var $fileList = "<ol>";
-  
-  for(var i=0; i<files.length; i++) {
-    $fileList = $fileList + `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-  }
-
-  $fileList = $fileList + "</ol>";
-  return $fileList;
-}
-
+var template = require('./lib/template.js');
 
 var app = http.createServer((request, response)=> {
   var _url = request.url;
@@ -54,9 +21,9 @@ var app = http.createServer((request, response)=> {
   const resHTML = (title, pCrTemp, pUpTemp, pDelTemp, pDescription)=> {
     fs.readdir('./data', 'utf8', (err, files)=> {
       fs.readFile('./data/' + queryData.id, 'utf8', (err, description)=> {
-        var templete = templeteHTML(title, templeteList(files), pCrTemp, pUpTemp, pDelTemp, `<h2>${title}</h2>${!pDescription?description:pDescription}`);
+        var html = template.html(title, template.list(files), pCrTemp, pUpTemp, pDelTemp, `<h2>${title}</h2>${!pDescription?description:pDescription}`);
         response.writeHead(200);
-        response.end(templete);
+        response.end(html);
       });
     });
   }
@@ -93,9 +60,9 @@ var app = http.createServer((request, response)=> {
           response.end('create process fail');
         }else {
           fs.readdir('./data', 'utf8', (err, files)=> {
-            var templete = templeteHTML(title, templeteList(files), '', '', `<h2>${title}</h2>${description}`);
+            var html = template.html(title, template.list(files), '', '', `<h2>${title}</h2>${description}`);
             response.writeHead(200);
-            response.end(templete);
+            response.end(html);
           });
         }
       });
@@ -136,9 +103,9 @@ var app = http.createServer((request, response)=> {
               response.end('update process writefile fail');
             }else {
               fs.readdir('./data', 'utf8', (err, files)=> {
-                var templete = templeteHTML(title, templeteList(files), $create, $update, $delete, `<h2>${title}</h2>${description}`);
+                var html = template.html(title, template.list(files), $create, $update, $delete, `<h2>${title}</h2>${description}`);
                 response.writeHead(200);
-                response.end(templete);
+                response.end(html);
               });
             }
           });
